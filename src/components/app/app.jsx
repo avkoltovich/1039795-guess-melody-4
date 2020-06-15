060f0fb16e67c75f8d377937a79048c34a368cf2
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-06-15 10:27:24 +0300

    feat: уточняет тип пропсов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
 const App = (props) => {
@@ -8,6 +9,10 @@ const App = (props) => {
   return (
     <WelcomeScreen errorsCount={errorsCount} />
   );
+};
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
 };
 
 export default App;
